78d5e17c720774053c8c59758e80ba93bcbf0a92
nahomHmichael <nahomhabtemichael@gmail.com>
2022-08-24 22:13:40 +0300

    Create fix_outliers.py

---
+++ scripts/fix_outliers.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def fix_outlier(df, column):
+    df[column] = np.where(df[column] > df[column].quantile(0.95), df[column].median(),df[column])
+    
+    return df[column]
+  
