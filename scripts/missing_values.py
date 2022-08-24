edbee4fe6cb4b33cae5b21f6c1ae87c92fb13cc7
nahomHmichael <nahomhabtemichael@gmail.com>
2022-08-24 12:28:08 +0300

    Finished fixing missing values

--- scripts/missing_values.py
+++ scripts/missing_values.py
@@ -13,3 +13,25 @@ def percent_missing(df):
 
     # Calculate percentage of missing values
     print("The dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
+
+def handle_missing_values(df):
+    perc = 30.0
+
+    min_amount = int(((100-perc)/100)*df.shape[0] +1)
+    modified_df = df.dropna(axis=1, thresh=min_amount)
+    #print("hello")
+    return modified_df
+def fill_missing_values(df):
+
+    obj_cols = [col_name for col_name in df.columns.tolist() if df.dtypes[col_name] == object]
+    numeric_cols = [col_name for col_name in df.columns.tolist() if col_name not in obj_cols]
+    #print(obj_cols)
+
+    #for object type columns use mode
+    for col in obj_cols:
+        df[col] = df[col].fillna(df[col].mode()[0])
+    
+    #for numeric type columns use median
+    for col in numeric_cols:
+        df[col] = df[col].fillna(df[col].median())
+    return df
