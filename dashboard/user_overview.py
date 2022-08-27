6cf7aca02a92d983dbe0884af2d3fd1fc3de8dcd
nahomHmichael <nahomhabtemichael@gmail.com>
2022-08-27 22:08:39 +0300

    Update: loading saved CSVs

--- dashboard/user_overview.py
+++ dashboard/user_overview.py
@@ -2,3 +2,15 @@ import streamlit as st
 import pandas as pd
 import os
 import sys
+
+
+def app():
+
+    st.title("User Experience Analysis")
+    
+    rtt = pd.read_csv('data/top10_rtt.csv')
+    tcp = pd.read_csv('data/top10tcp.csv')
+    frqnt_thrpt = pd.read_csv('data/most_freqAvgThr.csv')
+    frqnt_rtt = pd.read_csv('data/most_freqRTT.csv')
+    frqnt_tcp = pd.read_csv('data/most_freqTCP.csv')
+    avg_thrpt = pd.read_csv('data/top10avgThroughput.csv')
